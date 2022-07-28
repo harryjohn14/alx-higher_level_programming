468b61b857ee0833dd1aafb370cf450bbf084a48
Harrison <ugochukwujohn2014@gmail.com>
2022-07-28 01:39:05 -0700

    Updating_Task1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,7 +9,7 @@ def add_integer(a, b=98):
     """
 
     if ((not isinstance(a, int) and not isinstance(a, float))):
-        raise TypeError("a must be an integer")
+        raise TypeError("a must be an integer", end = "")
     if ((not isinstance(b, int) and not isinstance(b, float))):
-        raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+        raise TypeError("b must be an integer", end = "")
+    return (int(a) + int(b)i)
