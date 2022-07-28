eeda45c6a7ad6e85ff6d42b394d0d5f57325f505
Harrison <ugochukwujohn2014@gmail.com>
2022-07-28 04:44:55 -0700

    Python - Test-driven development

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 # 2-matrix_divided.py
 # John Harrison U <ugochukwujohn2014@gmail.com>
-"""Defines an integer addition function."""
-
+"""Defines a matrix division function."""
 
 
 def matrix_divided(matrix, div):
-    """ A function that divides all elements of a matrix
+    """Divide all elements of a matrix.
     Args:
-        matrix(list): A list of lists of integers or floats
-        div (integer/float): The divisor
+        matrix (list): A list of lists of ints or floats.
+        div (int/float): The divisor.
     Raises:
         TypeError: If the matrix contains non-numbers.
-        TypeError: If the matrix contains rows of different sizes
-        TypeError: If div is not an integer or float.
+        TypeError: If the matrix contains rows of different sizes.
+        TypeError: If div is not an int or float.
         ZeroDivisionError: If div is 0.
     Returns:
-        Anew matrix representing the result of the division.
+        A new matrix representing the result of the division.
     """
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
