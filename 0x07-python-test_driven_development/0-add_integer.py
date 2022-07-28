80b1fc1d0961edf141090c9496c57ed0c8722126
Harrison <ugochukwujohn2014@gmail.com>
2022-07-28 01:56:50 -0700

    Python - Test-driven development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,7 +9,7 @@ def add_integer(a, b=98):
     """
 
     if ((not isinstance(a, int) and not isinstance(a, float))):
-        raise TypeError("a must be an integer", end = "")
+        raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
-        raise TypeError("b must be an integer", end = "")
-    return (int(a) + int(b)i)
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
