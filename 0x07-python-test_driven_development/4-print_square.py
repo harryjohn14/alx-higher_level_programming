422d1905f7b51f9f2fe6d475f3884b5e2bdf7483
Harrison <ugochukwujohn2014@gmail.com>
2022-07-28 04:40:10 -0700

    Python - Test-driven development

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-# 0-add_integer.py
-# John Harrison U <ugochukwujohn2014@gmail.com>
-"""Defines an integer addition function."""
+# 4-print_square.py
+# John Harrison U <ugochukwujohn2014@gmail.com.com>
+"""Defines a square-printing function."""
+
 
 def print_square(size):
-    """ A function that prints a square with the character #.
+    """Print a square with the # character.
     Args:
-        size: is the size length of the square.
+        size (int): The height/width of the square.
     Raises:
-        TypeError: If size is not an integer
-        TypeError: If size is a float and is less than 0.
-        ValueError: If size is less than 0.
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    for x in range(size):
-        [print("#", end="") for y in range(size)]
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
         print("")
